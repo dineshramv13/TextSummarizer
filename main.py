0cf99640a0b194da53eff583117b7e935f914fa5
dineshramv13 <dineshramv13@gmail.com>
2024-11-22 14:09:16 +0530

    created data tranformation pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.TextSummarizer.logging import logger
 from src.TextSummarizer.pipeline.stage_1_dataingestion_pipeline import DataIngestionTrainingPipiline
+from src.TextSummarizer.pipeline.stage_2_datatransaformation_pipeline import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -7,6 +8,19 @@ try:
     logger.info(f"stage {STAGE_NAME} initiated")
     data_ingestion_pipeline = DataIngestionTrainingPipiline()
     data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"stage {STAGE_NAME} completed")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline = DataTransformationTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_transformation()
     logger.info(f"stage {STAGE_NAME} completed")
 
 except Exception as e:
